53fbfbe119355857717735a0b90c8cfeb4f9fa94
Franck <contact@franckwebpro.com>
2024-03-26 15:27:43 +0100

    mise en place props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,4 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-const pokemon = pokemonList[1];
-
-function PokemonCard() {
+function PokemonCard({pokemon}) {
     return <figure><figcaption>{pokemon.name}</figcaption>{pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}</figure>
 }
 
